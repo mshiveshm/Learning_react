fb111bfe2f8aee2586797028b5dc96271774bfe8
mshiveshm <2k22.csai.2212494@gmail.com>
2024-07-03 18:57:35 +0530

    make change

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 Learning react
+
+# Parcel
+- DEV Build
+- Local Server
+- HMR = Hot Module Replacement
+- File Watching Algorithm - written in C++
+- Caching - Faster Builds
+- Image Optimization
+- Minification
+- Bundling
+- Compressing 
+- Consistent Hashing
+- Code splitting
+- Differential Bundling - support older browsers
+- Diagnostic Features
+- Error Handling
+- Tree Shaking - remove unused code 
--- app.js
+++ app.js
@@ -1,80 +1,36 @@
-// React.createElement("",{},"") --> It is nothing just a javascript object and it is not any HTML Tag.
-// const heading=React.createElement(
-//     "h1",
-//     {id: "heading"},   // for the attributes
-//     "Welcome to React")
-
-
-/*
-<div id="parent">
-<div id="child">
-    <h1>I'm a h1 tah</h1>
-</div>
-</div>
-*/
-
-// const parent=React.createElement(
-//         "div",
-//         {id:"parent"},
-//         React.createElement(
-//             "div",
-//             {id:"child"},
-//             React.createElement("h1",{},"I'm a h1 tag")))    
-// const root=ReactDOM.createRoot(document.getElementById("root"))
-// // render() method is responsible to take this js object and convert it into h1 tag and put it up
-// //root.render(heading)
-// root.render(parent)
-
-
-/*
-<div id="parent">
-<div id="child">
-    <h1>I'm a h1 tah</h1>
-    <h2>I'm h2</h2>
-</div>
-</div>
-*/
-
-
-// const parent=React.createElement(
-//     "div",
-//     {id:"parent"},
-//     React.createElement(
-//         "div",
-//         {id:"child"},
-//         [React.createElement("h1",{},"I'm a h1 tag"), 
-//         React.createElement("h2",{},"I'm a h2 tag")]))    
-// const root=ReactDOM.createRoot(document.getElementById("root"))
-// root.render(parent)
-
-/*
-<div id="parent">
-<div id="child1">
-    <h1>I'm a h1 tah</h1>
-    <h2>I'm h2</h2>
-</div>
-<div id="child2">
-    <h1>I'm a h1 tah</h1>
-    <h2>I'm h2</h2>
-</div>
-</div>
-*/
-
+import React from "react";
+import ReactDOM from "react-dom/client";
+// React.createElement => object =>HTML Element(render)
 const parent=React.createElement(
     "div",
     {id:"parent"},
-    [
-        React.createElement(
-        "div",
-        {id:"child"},
-        [React.createElement("h1",{},"I'm a h1 tag"), 
-        React.createElement("h2",{},"I'm a h2 tag")]),
+    "Shivesh is using"
+)  
+    console.log(parent);
+    // JSX (transpiled before it reaches the JS) - PARCEL - Babel
+    
+    // JSX => React.createElement => ReactElement-JS Object => HTMLElement(render)
+    const heading = () => (
+    <h1 className="head" tabIndex="5">
+        hey Shivesh! using JSX
+        </h1>
+    );
+    // React functional Component
+    // Class Based Component - Old
+    // Functional Component - New (A JS function that return the JSX Code is called the functional Component)
+    
+    // const HeadingComponent1 = () =>{
+    //     return <h1>Functional Component</h1>
+    // }
+// Functional Component  
+  const number=100000;
+    const HeadingComponent = () => (
+        <div id="container">
+            {parent}
+            <h2>{number+100}</h2>
+        <h1>hey Shivesh using Functional Component</h1>
+        </div>
+    );
 
-        React.createElement(
-            "div",
-            {id:"child"},
-            [React.createElement("h1",{},"I'm a h1 tag"), 
-            React.createElement("h2",{},"I'm a h2 tag")])
-    ])    
-const root=ReactDOM.createRoot(document.getElementById("root"))
-root.render(parent)
+const root=ReactDOM.createRoot(document.getElementById("root"));
+root.render(<HeadingComponent/>)
